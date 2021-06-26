462589765f6290020390a2c42a1226b1d3aa5d1f
Jan-E <github@ehrhardt.nl>
2021-06-26 18:25:44 +0200

    Build static avif_a.lib and aom_a.lib
    Closes GH-2.

--- .github/workflows/build.ps1
+++ .github/workflows/build.ps1
@@ -25,10 +25,11 @@ $on = if ($architecture -eq "x64") {"1"} else {"0"}
 cmake "-G" "$generator" "-A" "$platform" "-DENABLE_DOCS=0" "-DENABLE_EXAMPLES=0" "-DENABLE_TESTDATA=0" "-DENABLE_TESTS=0" "-DENABLE_TOOLS=0" "-DENABLE_NASM=1" "-DENABLE_SSE2=$on" "-DENABLE_SSE3=$on" "-DENABLE_SSSE3=$on" "-DENABLE_SSE4_1=$on" "-DENABLE_SSE4_2=$on" "-DENABLE_AVX=$on" "-DENABLE_AVX2=$on" ".."
 msbuild "/t:Build" "/p:Configuration=Release" "/p:Platform=$platform" "AOM.sln"
 xcopy "Release\*.lib" "."
+xcopy "Release\aom.lib" ".\aom_a.lib*"
+xcopy ".\aom_a.lib" "..\..\..\winlibs\lib\"
 Set-Location "..\..\.."
 
-cmake "$generator" -A "$platform" -DAVIF_CODEC_AOM=1 -DAVIF_LOCAL_AOM=1 -DAVIF_ENABLE_WERROR=0 .
-msbuild "/t:Build" "/p:Configuration=Release" "/p:Platform=$platform" "libavif.sln"
-xcopy "Release\avif.dll" "winlibs\bin\*"
+cmake "$generator" -A "$platform" -DAVIF_CODEC_AOM=1 -DAVIF_LOCAL_AOM=1 -DAVIF_ENABLE_WERROR=0 -DBUILD_SHARED_LIBS=0 .
+msbuild "/t:Build" "/p:Configuration=RelWithDebInfo" "/p:Platform=$platform" "libavif.sln"
 xcopy "include\avif\avif.h" "winlibs\include\avif\*"
-xcopy "Release\avif.lib" "winlibs\lib\*"
+xcopy "RelWithDebInfo\avif*.??b" "winlibs\lib\*"
